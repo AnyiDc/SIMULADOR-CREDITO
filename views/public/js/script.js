e135a3b1b27618afad10669b7f577cc36b3dbd53
AnyiDc <anyi.katerine@hotmail.com>
2024-11-13 15:49:17 -0500

    switch y agregando clases

--- views/public/js/script.js
+++ views/public/js/script.js
@@ -1,66 +1,82 @@
-// function Persona(nombre, edad, estatura, color) {
-//     this.nombre = nombre
-//     this.edad = edad
-//     this.estatura = estatura
-//     this.color = color
-//     this.moverse = function (nombre) {
-//         this.nombre = nombre
-//         console.log(nombre + ' Se movió')
-//     }
-//     this.expresarse = function () {
-//         console.log('Se expresó...')
-//     }
-// }
-// let personaUno = new Persona('Laura', 18, 1.55, 'Blanco')
-// let personaDos = new Persona('Daniel', 18, 1.74, 'Blanco')
-// let personaTres = new Persona('Anyis', 18, 1.60, 'Blanco')
-// personaUno.moverse()
-// personaDos.moverse()
-// personaTres.moverse()
-// console.log(personaUno, personaDos, personaTres)
+import { expresionesRegulares } from "../../../helpers/expresiones.js";
 
-// let persona = {
-//     nombre: 'Juan Felipe',
-//     apellido: 'Niño',
-//     edad: 19,
-//     moverse: function () {
-//         console.log('Se esta moviendo...')
-//     },
-//     direccion: {
-//         calle: 23,
-//         carrera: 23,
-//         interior: 20
-//     }
-// }
+// console.log(expresionesRegulares.nombre);
 
-document.querySelector('#btnFormRegistro').addEventListener('click', function () {
-    document.querySelector('#form-login').style.display = 'flex'
-    document.querySelector('#form-register').style.display = 'none'
-})
-document.querySelector('#btnFormLogin').addEventListener('click', function () {
-    document.querySelector('#form-login').style.display = 'none'
-    document.querySelector('#form-register').style.display = 'flex'
-})
-let inputs = document.getElementsByTagName('input')
+document
+    .querySelector("#btnFormRegistro")
+
+    .addEventListener("click", function () {
+        document.querySelector("#form-login").style.display = "flex";
+        document.querySelector("#form-register").style.display = "none";
+    });
+document.querySelector("#btnFormLogin").addEventListener("click", function () {
+    document.querySelector("#form-login").style.display = "none";
+    document.querySelector("#form-register").style.display = "flex";
+});
+let inputs = document.getElementsByTagName("input");
 for (let index = 0; index < inputs.length; index++) {
-    inputs[index].addEventListener('keyup', validarFormulario)
+    inputs[index].addEventListener("keyup", validarFormulario);
 }
 function validarFormulario(e) {
+    //   console.log(e);
     switch (e.target.name) {
-        case 'login-usuario':
-            console.log('Este es el input de login usuario')
+        case "register-nombre":
+            if (expresionesRegulares.nombre.test(e.target.value)) {
+                document.getElementById("register-nombre").classList.add("correcto");
+                document
+                    .getElementById("register-nombre")
+                    .classList.remove("incorrecto");
+                // document.getElementById("mensaje-nombre").textContent =
+                // "";
+            } else {
+                document.getElementById("register-nombre").classList.add("incorrecto");
+                document.getElementById("register-nombre").classList.remove("correcto");
+                // document.getElementById("mensaje-nombre").textContent =
+                //   "El nombre solo puede ir en mayúsculas";
+            }
             break;
-
-        default:
+        case "register-usuario":
+            if (expresionesRegulares.usuario.test(e.target.value)) {
+                document.getElementById("register-usuario").classList.add("correcto");
+                document.getElementById("register-usuario").classList.remove("incorrecto");
+                document.getElementById("mensaje-nombre").textContent =
+                    "El nombre solo permite mayúsculas";
+            } else {
+                document.getElementById("register-usuario").classList.add("incorrecto");
+                document.getElementById("register-usuario").classList.remove("correcto");
+            }
+            break;
+        case "register-correo":
+            if (expresionesRegulares.correo.test(e.target.value)) {
+                document.getElementById("register-correo").classList.add("correcto");
+                document.getElementById("register-correo").classList.remove("incorrecto");
+            } else {
+                document.getElementById("register-correo").classList.add("incorrecto");
+                document.getElementById("register-correo").classList.remove("correcto");
+            }
+            break;
+        case "register-contrasena":
+            if (expresionesRegulares.contraseña.test(e.target.value)) {
+                document.getElementById("register-contrasena").classList.add("correcto");
+                document.getElementById("register-contrasena").classList.remove("incorrecto");
+            } else {
+                document.getElementById("register-contrasena").classList.add("incorrecto");
+                document.getElementById("register-contrasena").classList.remove("correcto");
+            }
+            break;
+        case "register-confirmar":
+            if (expresionesRegulares.contraseña.test(e.target.value)) {
+                document.getElementById("register-confirmar").classList.add("correcto");
+                document.getElementById("register-confirmar").classList.remove("incorrecto");
+            } else {
+                document.getElementById("register-confirmar").classList.add("incorrecto");
+                document.getElementById("register-confirmar").classList.remove("correcto");
+            }
             break;
     }
 }
 
 // console.log(document.querySelectorAll('input'))
-
-
-
-
 // let arreglo = [1, 'jaime', true, function () { }, { nombre: 'jaime' }, []]
 // console.log(arreglo)
 // console.log(arreglo[4])
